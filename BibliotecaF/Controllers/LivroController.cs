ddf1b15221d546e0d99b1732797069cac137ece7
FelipeOliver00 <Felipecastro0208@hotmail.com>
2020-12-29 18:58:12 -0300

    Implementação da Biblioteca

---
+++ BibliotecaF/Controllers/LivroController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BibliotecaF.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BibliotecaF.Controllers
+{
+    public class LivroController : Controller
+    {
+
+        //Simulando banco de dados da Biblioteca _bancob
+        private static List<Livro> _bancob = new List<Livro>();
+        
+        //Listando Livros
+        public IActionResult Index()
+        {
+            //Enviar a lista de livros
+            return View(_bancob);
+
+        }
+
+
+    
+        //Removendo Livro
+        public IActionResult Remover(int id)
+        {
+            //Removendo
+            _bancob.RemoveAt(_bancob.FindIndex(liv => liv.Codigo == id));
+            //Mensagem de sucesso
+            TempData["msg"] = "Livro Excluído";
+            //Redirect
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Livro livro)
+        {
+            //Validando Usuário do _bancob
+            //Editando Livro (usando index do livro e subistituindo o objeto)
+            _bancob[ _bancob.FindIndex(liv => liv.Codigo == livro.Codigo)] = livro;
+            //Mensagem de sucesso
+            TempData["msg"] = "Livro Atualizado!";
+            //Redierct para listagem
+            return RedirectToAction("Index");
+        }
+
+        //Recebendo id do Livro e retorna os valores do livro para view
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            //Procurando livro com código
+            var liv = _bancob.Find(l=> l.Codigo == id);
+
+            //Enviando o livro para view objeto liv
+            return View(liv);
+        }
+
+        //Cadastrando o Livro e retornando msg success
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            livro.Codigo = _bancob.Count + 1;
+            _bancob.Add(livro);
+            TempData["msg"] = "Livro registrado na Biblioteca";
+            return RedirectToAction("cadastrar");
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ BibliotecaF/Models/Livro.cs
@@ -0,0 +1,40 @@
+﻿using BibliotecaF.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaF.Models
+{
+    public class Livro
+    {
+        [HiddenInput]
+        public int Codigo { get;  set; }
+
+        [Display(Name = "Autor do Livro")]
+        public string Autor { get; set; }
+
+        [Display(Name = "Título do Livro")]
+        public string Titulo { get; set; }
+
+        [Display(Name = "Editora do Livro")]
+        public string   Editora { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Publicação")]
+        public DateTime DataPublicacao { get; set; }
+
+        [Display(Name = "Tipo de Livro:")]
+        public Tipo Tipo { get; set; }
+
+
+        [Display(Name = "Livro velho")]
+        public  Boolean Condicao  { get; set; }
+
+        [Display(Name ="Preço do Livro")]
+        public decimal Preco { get; set; }
+        
+    }
+}
